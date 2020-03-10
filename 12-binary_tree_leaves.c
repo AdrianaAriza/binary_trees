69e42774f8bd587b7096f5b2baf2ac38a3f933c6
JDorangetree <1289@holbertonschool.com>
2020-03-10 00:18:37 +0000

    [feature] upload all files

---
+++ 12-binary_tree_leaves.c
@@ -0,0 +1,11 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_leaves - function that counts the leaves in a binary tree
+ * @tree: pointer to the root node of the tree to count the number of leaves
+ *
+ * Return: If tree is NULL, your function must return 0
+ */
+size_t binary_tree_leaves(const binary_tree_t *tree)
+{
+
+}
---
+++ 18-binary_tree_uncle.c
@@ -0,0 +1,11 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_uncle - function that finds the uncle of a node
+ * @node: pointer to the node to find the uncle
+ *
+ * Return: If node is NULL, return NULL
+ */
+binary_tree_t *binary_tree_uncle(binary_tree_t *node)
+{
+
+}
