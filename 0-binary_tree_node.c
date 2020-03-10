4e7126503c3deb75a17effbbff25d49fef61e443
JDorangetree <1289@holbertonschool.com>
2020-03-10 01:20:27 +0000

    [Feature] upload task 0

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -8,5 +8,15 @@
  */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
+    struct binary_tree_s *new_node;
 
+    new_node = malloc(sizeof(struct binary_tree_s));
+	if (!new_node)
+		return (NULL);
+    
+    new_node->parent = parent;
+    new_node->n = value;
+    new_node->right = NULL;
+    new_node->left = NULL;
+    return(new_node);
 }
