242d37268962689bcd97e484b12a4db8ec9792e6
JDorangetree <julian.naranjo2014@gmail.com>
2020-03-11 16:35:59 -0500

    [Fix] balanced

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -13,10 +13,40 @@ int binary_tree_balance(const binary_tree_t *tree)
 	if (!tree)
 		return (0);
 	if (tree->right)
-		hr = 1 + binary_tree_balance(tree->right);
+	{
+		hr = binary_tree_height(tree->right);
+		printf("%d", hr);
+	}
+	else
+		hr = -1;
 	if (tree->left)
 	{
-		hl = 1 + binary_tree_balance(tree->left);
+		hl = binary_tree_height(tree->left);
+		printf("%d", hr);
 	}
+	else
+		hl = -1;
 	return (hl - hr);
+}
+/**
+ * binary_tree_height - function that measures the height of a binary tree
+ * @tree: pointer to the root node of the tree to measure the height.
+ *
+ * Return: If tree is NULL, your function must return 0
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t hl = 0, hr = 0;
+
+	if (!tree)
+		return (0);
+	if (tree->right)
+		hr = 1 + binary_tree_height(tree->right);
+	if (tree->left)
+	{
+		hl = 1 + binary_tree_height(tree->left);
+	}
+	if (hr > hl)
+		return (hr);
+	return (hl);
 }
