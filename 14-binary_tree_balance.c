8a036da246402d39a004417dd8056d1d8c12f771
JDorangetree <julian.naranjo2014@gmail.com>
2020-03-11 15:04:53 -0500

    [Feature] upload task 14

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,11 +1,22 @@
 #include "binary_trees.h"
 /**
- * binary_tree_balance - function that measures the balance factor of a binary tree
+ * binary_tree_balance - function that measures
+ * the balance factor of a binary tree
  * @tree: pointer to the root node of the tree to measure the balance factor
  *
  * Return: If tree is NULL, return 0
  */
 int binary_tree_balance(const binary_tree_t *tree)
 {
+	size_t hl = 0, hr = 0;
 
+	if (!tree)
+		return (0);
+	if (tree->right)
+		hr = 1 + binary_tree_balance(tree->right);
+	if (tree->left)
+	{
+		hl = 1 + binary_tree_balance(tree->left);
+	}
+	return (hl - hr);
 }
