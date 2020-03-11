f0474dca5a92c09d8e2868816cfc694e091b6860
AdrianaAriza <1296@holbertonschool.com>
2020-03-11 12:57:39 -0500

    [Fix]: order

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -9,11 +9,11 @@
  */
 void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
 {
-	func(tree->n);
 	if (!tree || !func)
 	{
 		return;
 	}
+	func(tree->n);
 	if (tree->left)
 	{
 		binary_tree_preorder(tree->left, func);
