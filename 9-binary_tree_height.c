ca45cec66e3de6d15dfd292a5ce2bf85ae11e7fb
AdrianaAriza <1296@holbertonschool.com>
2020-03-11 14:21:32 -0500

    [Fix]: If null

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -9,7 +9,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
 {
 	size_t hl = 0, hr = 0;
 
-	if(!tree)
+	if (!tree)
 		return (0);
 	if (tree->right)
 		hr = 1 + binary_tree_height(tree->right);
