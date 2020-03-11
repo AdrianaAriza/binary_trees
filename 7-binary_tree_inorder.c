946708a0efa72060bc57f7b35a57a4d589b34b13
AdrianaAriza <1296@holbertonschool.com>
2020-03-11 13:58:52 -0500

    [Fix]: right

--- 7-binary_tree_inorder.c
+++ 7-binary_tree_inorder.c
@@ -18,6 +18,6 @@ void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
 	func(tree->n);
 	if (tree->right && (tree->right->left || tree->right->right))
 		binary_tree_inorder(tree->right, func);
-	else if (tree->right)
+	else if (tree->right && !(tree->right->left || tree->right->right))
 		func(tree->right->n);
 }
